e358b4ce3bd00a85d6a95d955ae869d2fe2e6433
Rolland Hugo <56535478+Yug0h@users.noreply.github.com>
2019-11-04 10:54:48 +0100

    Update Pfsense.py

--- Pfsense.py
+++ Pfsense.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" create by HVG0 on 02/19
+""" create by Yug0h on 02/19
     This script allow u to creat cert on pfsense
     This is V2 in  OOP	
 """
